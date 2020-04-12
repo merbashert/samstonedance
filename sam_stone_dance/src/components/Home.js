bc385928fba2211e505e6b8108eef9dbdbcfc9bd
Meredith Bashert <merbashert@gmail.com>
2020-04-12 10:16:13 -0400

    Animated front page

--- sam_stone_dance/src/components/Home.js
+++ sam_stone_dance/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import banner from '../assets/Sam_banner.jpg'
+import moon from '../assets/moon.png'
 import Image from 'react-bootstrap/Image';
 
 
@@ -7,9 +8,8 @@ const Home = props => {
 
     return (
         <div className="sitename-container">
-
-                <Image src={banner} alt='banner'/>
-
+                <Image src={banner} alt='banner' id='banner'/>
+                <Image src={moon} alt='moon' id = 'moon'/>
         </div>
     )
 }
