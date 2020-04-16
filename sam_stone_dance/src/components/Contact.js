9e14afba7fa59bae60dc7d3edb00cdf375a6b8ad
Meredith Bashert <merbashert@gmail.com>
2020-04-16 13:33:25 -0400

    Built contact page

--- sam_stone_dance/src/components/Contact.js
+++ sam_stone_dance/src/components/Contact.js
@@ -5,8 +5,12 @@ class Contact extends React.Component {
     render () {
         return(
             <div className='contact-container'>
-            <img src = {moon} className = 'moon_drop' id='moon_contact'></img>
-            <h1><a href = 'mailto:samstone16@gmail.com'>samstone16@gmail.com</a></h1>
+                <img src = {moon} className = 'moon_drop' id='moon_contact'></img>
+                <div className='contact-info'>
+                    <h2><a href = 'mailto:samstone16@gmail.com'>samstone16@gmail.com</a></h2>
+                    <br/>
+                    <h2><a href = 'https://www.instagram.com/otherdance' target="_blank">Sam's Instagram</a></h2>
+                </div>
             </div>
         )
     }
