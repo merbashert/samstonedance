513aadcb02109be84dbf39e2a728c5e1fd7670f3
Meredith Bashert <merbashert@gmail.com>
2020-04-17 11:53:17 -0400

    Built image carousels

--- sam_stone_dance/src/components/Project.js
+++ sam_stone_dance/src/components/Project.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel'
 
-export class Project_Evening extends React.Component {
+export class Project extends React.Component {
     state = {
         imgUrl: '',
+        images:{},
         isLoaded: false
     }
 
@@ -13,34 +15,67 @@ export class Project_Evening extends React.Component {
     }
 
     componentDidMount() {
-        const { featured_media } = this.props.project;
+        const { featured_media, id } = this.props.project;
         const getImageUrl = axios.get(`http://localhost:8000/wp-json/wp/v2/media/${featured_media}`);
+        const getImages = axios.get(`http://localhost:8000/wp-json/wp/v2/media?parent=${id}`);
 
-        Promise.all([getImageUrl]).then(res => {
+        Promise.all([getImageUrl, getImages]).then(res => {
             this.setState({
                 imgUrl: res[0].data.media_details.sizes.full.source_url,
+                images: res[1].data,
                 isLoaded: true
             })
         });
+
     }
+
     render () {
         const { title } = this.props.project;
         const { choreographer, performed_by, music_by, performance_date, video_excerpt } = this.props.project.acf;
-        const { imgUrl, isLoaded} = this.state;
+        const { imgUrl, images, isLoaded} = this.state;
+        const imageArray = [];
+        for(let i = 0; i < images.length; i++) {
+            imageArray.push(images[i].media_details.sizes.full.source_url)
+        }
         if(isLoaded) {
             return (
                 <div className='project'>
-                <img src = {imgUrl} alt={title.rendered} className='project-image'></img>
-                <div className = 'project-info'>
-                <h3>{ title.rendered }</h3>
-                <h5>{ choreographer }</h5>
-                <h5>{ performed_by }</h5>
-                <h5>{ music_by }</h5>
-                <h5>{ performance_date }</h5>
-                {video_excerpt?<a href={video_excerpt} className='btn btn-outline-dark'><h5>Video Excerpt</h5></a>:null}
+                    <Carousel indicators='false'>
+                        {imageArray.map((image, id) => {
+                            return imageArray.length > 1 ?
 
-                </div>
-                <hr/>
+                                <Carousel.Item key={image.id}>
+                                    <div className='project-image'>
+                                    <img src= {image} style={{width:"80%"}} alt={title.rendered}/>
+                                    </div>
+                                </Carousel.Item>
+
+                            :
+                            <div className='project-image'>
+                            <img src = {imgUrl} style={{width:"80%"}}alt={title.rendered}/>
+                            </div>
+                        })}
+
+
+                    </Carousel>
+
+
+
+
+
+
+
+
+                    <div className = 'project-info'>
+                        <h3>{ title.rendered }</h3>
+                        <h5>{ choreographer }</h5>
+                        <h5>{ performed_by }</h5>
+                        <h5>{ music_by }</h5>
+                        <h5>{ performance_date }</h5>
+                        {video_excerpt?<a href={video_excerpt} className='btn btn-outline-dark'><h5>Video Excerpt</h5></a>:null}
+
+                    </div>
+                    <hr/>
                 </div>
             )
         }
@@ -48,4 +83,4 @@ export class Project_Evening extends React.Component {
     }
 }
 
-export default Project_Evening;
+export default Project;
