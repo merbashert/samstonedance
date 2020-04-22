59e8445b6f38fb29b8a8f08ac21da91e4063a481
Meredith Bashert <merbashert@gmail.com>
2020-04-22 16:34:27 -0400

    Added footer, restyled navbar

--- sam_stone_dance/src/App.js
+++ sam_stone_dance/src/App.js
@@ -26,63 +26,66 @@ const App = props => {
 
     const [expanded, setExpanded] = useState(false);
 
-let content = (
+    let content = (
         <Router>
-                <Navbar className='nav-bar' sticky="top" expand="lg" expanded={expanded}>
-                    <Navbar.Brand id='brand'>Sam Stone Dance</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")}/>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <div className='navlink'>
-                                <Nav.Link  as = {NavLink} to="/samstonedance/">Home</Nav.Link>
-                            </div>
-                            <div className='navlink'>
-                                <Nav.Link as = {NavLink} to="/samstonedance/about" onClick={() => setTimeout(() => {setExpanded(false)}, 150)}>About</Nav.Link>
-                            </div>
-                            <div className='navlink'>
-                                <Nav.Link as = {NavLink} to="/samstonedance/current-projects" onClick={() => setTimeout(() => {setExpanded(false)}, 150)}>Current Projects</Nav.Link>
-                            </div>
-                            <div className='navlink'>
-                                <Nav.Link as = {NavLink} to="/samstonedance/sam-creates" onClick={() => setTimeout(() => {setExpanded(false)}, 150)}>Sam Creates</Nav.Link>
-                            </div>
-                            <div className='navlink'>
-                                <NavDropdown title = "Sam Teaches">
-                                    <Nav.Link  as = {NavLink} to="/samstonedance/sam-teaches-adult">Adult Classes</Nav.Link>
-                                    <Nav.Link as = {NavLink}  to="/samstonedance/sam-teaches-child">Child Classes</Nav.Link>
-                                </NavDropdown>
-                            </div>
-                            <div className='navlink'>
-                                <Nav.Link  as = {NavLink} to="/samstonedance/contact" onClick={() => setTimeout(() => {setExpanded(false)}, 150)}>Contact</Nav.Link>
-                            </div>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+            <Navbar className='nav-bar' sticky="top" expand="lg" expanded={expanded}>
+                <Navbar.Brand id='brand'>Sam Stone Dance</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")}/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <div className='navlink'>
+                            <Nav.Link  as = {NavLink} to="/samstonedance/">Home</Nav.Link>
+                        </div>
+                        <div className='navlink'>
+                            <Nav.Link as = {NavLink} to="/samstonedance/about" onClick={() => setTimeout(() => {setExpanded(false)}, 150)}>About</Nav.Link>
+                        </div>
+                        <div className='navlink'>
+                            <Nav.Link as = {NavLink} to="/samstonedance/current-projects" onClick={() => setTimeout(() => {setExpanded(false)}, 150)}>Current Projects</Nav.Link>
+                        </div>
+                        <div className='navlink'>
+                            <Nav.Link as = {NavLink} to="/samstonedance/sam-creates" onClick={() => setTimeout(() => {setExpanded(false)}, 150)}>Sam Creates</Nav.Link>
+                        </div>
+                        <div className='navlink'>
+                            <NavDropdown title = "Sam Teaches">
+                                <Nav.Link  as = {NavLink} to="/samstonedance/sam-teaches-adult">Adult Classes</Nav.Link>
+                                <Nav.Link as = {NavLink}  to="/samstonedance/sam-teaches-child">Child Classes</Nav.Link>
+                            </NavDropdown>
+                        </div>
+                        <div className='navlink'>
+                            <Nav.Link  as = {NavLink} to="/samstonedance/contact" onClick={() => setTimeout(() => {setExpanded(false)}, 150)}>Contact</Nav.Link>
+                        </div>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
 
 
-                <Switch>
-                    <Route exact path="/samstonedance/">
-                        <Home/>
-                    </Route>
-                    <Route path="/samstonedance/about">
-                        <About />
-                    </Route>
-                    <Route path="/samstonedance/current-projects">
-                        <CurrentProjects />
-                    </Route>
-                    <Route path="/samstonedance/sam-creates">
-                        <SamCreates />
-                    </Route>
-                    <Route path="/samstonedance/sam-teaches-adult">
-                        <SamTeachesAdult />
-                    </Route>
-                    <Route path="/samstonedance/sam-teaches-child">
-                        <SamTeachesChild />
-                    </Route>
-                    <Route path="/samstonedance/contact">
-                        <Contact />
-                    </Route>
-                </Switch>
-
+            <Switch>
+                <Route exact path="/samstonedance/">
+                    <Home/>
+                </Route>
+                <Route path="/samstonedance/about">
+                    <About />
+                </Route>
+                <Route path="/samstonedance/current-projects">
+                    <CurrentProjects />
+                </Route>
+                <Route path="/samstonedance/sam-creates">
+                    <SamCreates />
+                </Route>
+                <Route path="/samstonedance/sam-teaches-adult">
+                    <SamTeachesAdult />
+                </Route>
+                <Route path="/samstonedance/sam-teaches-child">
+                    <SamTeachesChild />
+                </Route>
+                <Route path="/samstonedance/contact">
+                    <Contact />
+                </Route>
+            </Switch>
+            <footer>
+                <h3 className='instagram'><span className='fa fa-instagram'></span><a href = 'https://www.instagram.com/otherdance' target="_blank" rel="noopener noreferrer">@otherdance</a></h3>
+                <h3 className = 'mail'><span className='fa fa-envelope-o'></span><a href = 'mailto:samstone16@gmail.com'>samstone16@gmail.com</a></h3>
+            </footer>
         </Router>
     );
     return content;
