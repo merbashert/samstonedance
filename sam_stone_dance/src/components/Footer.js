2c5e3381338a731ce0e37cdb1d95faddd3bcd09a
Meredith Bashert <merbashert@gmail.com>
2020-04-22 17:56:35 -0400

    Fixed footer spacing

--- sam_stone_dance/src/components/Footer.js
+++ sam_stone_dance/src/components/Footer.js
@@ -2,24 +2,26 @@ import React from 'react';
 
 const footer = (props) => (
     <div className = 'footer'>
-        <h3 className='instagram'>
-            <a href = 'https://www.instagram.com/otherdance' target="_blank" rel="noopener noreferrer">
-                <span className='fa fa-instagram'>
-                </span>
-            </a>
-        </h3>
-        <h3 className = 'mail'>
-            <a href = 'mailto:samstone16@gmail.com'>
-                <span className='fa fa-envelope-o'>
-                </span>
-            </a>
-        </h3>
-        <h3 className = 'facebook'>
-            <a href = 'https://www.facebook.com/Sam-Stone-Dance-108382910851063'>
-                <span className='fa fa-facebook'>
-                </span>
-            </a>
-        </h3>
+        <div className = 'social'>
+            <h3 className='instagram'>
+                <a href = 'https://www.instagram.com/otherdance' target="_blank" rel="noopener noreferrer">
+                    <span className='fa fa-instagram'>
+                    </span>
+                </a>
+            </h3>
+            <h3 className = 'mail'>
+                <a href = 'mailto:samstone16@gmail.com'>
+                    <span className='fa fa-envelope-o'>
+                    </span>
+                </a>
+            </h3>
+            <h3 className = 'facebook'>
+                <a href = 'https://www.facebook.com/Sam-Stone-Dance-108382910851063'>
+                    <span className='fa fa-facebook'>
+                    </span>
+                </a>
+            </h3>
+        </div>
     </div>
 )
 
