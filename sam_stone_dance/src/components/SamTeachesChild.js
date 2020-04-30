c7916e9592163b18d9830a9a05a9da901d467d4d
Meredith Bashert <merbashert@gmail.com>
2020-04-30 15:08:03 -0400

    Got Sam teaches child image from embedded

--- sam_stone_dance/src/components/SamTeachesChild.js
+++ sam_stone_dance/src/components/SamTeachesChild.js
@@ -5,24 +5,32 @@ import axios from 'axios';
 class SamTeachesChild extends React.Component {
     state = {
         childTeach: '',
+        image: '',
         isLoaded: false
     }
 
     componentDidMount() {
-        axios.get('http://axi.smv.mybluehost.me/wp-json/wp/v2/posts/113')
-        .then(res => this.setState({
-            childTeach: res.data,
-            isLoaded: true
-        }))
-        .catch(err => console.log(err));
+        const getChildTeach = axios.get('http://axi.smv.mybluehost.me/wp-json/wp/v2/posts/113?_embed')
+
+
+        Promise.all([getChildTeach]).then(res => {
+            this.setState({
+                childTeach: res[0].data,
+                image: res[0].data._embedded['wp:featuredmedia']['0'].source_url,
+                isLoaded: true
+            })
+        }).catch(err => console.log(err));
     }
 
 
     render () {
-        const { childTeach, isLoaded } = this.state;
+        const { childTeach, image, isLoaded } = this.state;
         if(isLoaded) {
             return(
                 <div className='child-teach'>
+                    <h3>Creative Dance For Kids</h3>
+                    <hr />
+                    <img src = {image} alt = 'Sam black and white' className = 'child-teach-photo'/>
                 <h6 dangerouslySetInnerHTML = {{__html: childTeach.content.rendered}}></h6>
                 </div>
             )
