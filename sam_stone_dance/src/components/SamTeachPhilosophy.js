3003f406ca71e4fe31669d34c8d5475ad728a5ca
Meredith Bashert <merbashert@gmail.com>
2020-04-30 15:08:21 -0400

    Got Sam teach philosophy image from embedded, made image bg

--- sam_stone_dance/src/components/SamTeachPhilosophy.js
+++ sam_stone_dance/src/components/SamTeachPhilosophy.js
@@ -7,27 +7,38 @@ import cv from '../assets/CV.pdf'
 class SamTeachPhilosophy extends React.Component {
     state = {
         teachPhilosophy: '',
+        image: '',
         isLoaded: false
     }
 
     componentDidMount() {
-        axios.get('http://axi.smv.mybluehost.me/wp-json/wp/v2/posts/109')
-        .then(res => this.setState({
-            teachPhilosophy: res.data,
-            isLoaded: true
-        }))
-        .catch(err => console.log(err));
+        const getTeachPhilosophy = axios.get('http://axi.smv.mybluehost.me/wp-json/wp/v2/posts/109?_embed')
+
+
+        Promise.all([getTeachPhilosophy]).then(res => {
+            this.setState({
+                teachPhilosophy: res[0].data,
+                image: res[0].data._embedded['wp:featuredmedia']['0'].source_url,
+                isLoaded: true
+            })
+        }).catch(err => console.log(err));
     }
 
-
     render () {
-        const { teachPhilosophy, isLoaded } = this.state;
+        const { teachPhilosophy, image, isLoaded } = this.state;
         if(isLoaded) {
             return(
-                <div className='teaching-philosophy'>
+                <div className='teaching-philosophy'
+                    style={{
+                        backgroundImage: `url(${image})`,
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat'
+                    }}>
+                    <h3>Teaching Philosophy</h3>
                     <h6 dangerouslySetInnerHTML = {{__html: teachPhilosophy.content.rendered}}></h6>
 
-                    <h4 className='cv'><span className='fa fa-arrow-circle-o-down'></span><a href = {cv} download>Download Teaching CV</a></h4>
+                    <h6 className='cv'><span className='fa fa-arrow-circle-o-down'></span><a href = {cv} download>Download Teaching CV PDF</a></h6>
                 </div>
             )
         }
