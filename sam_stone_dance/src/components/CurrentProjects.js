42a765f29c289d1cb2bb05c3f76d7c93c5e47767
Meredith Bashert <merbashert@gmail.com>
2020-05-01 09:04:37 -0400

    Changed out YouTube frame

--- sam_stone_dance/src/components/CurrentProjects.js
+++ sam_stone_dance/src/components/CurrentProjects.js
@@ -4,7 +4,7 @@ class CurrentProjects extends React.Component {
     render () {
         return(
             <div className = 'current-projects'>
-            <iframe title='dance_reel' width="640" height="385" id='youtube' src="https://www.youtube.com/embed/jhMEFpUtJhs" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe title='dance_reel' width="840" height="505" id='youtube' src="https://www.youtube.com/embed/jhMEFpUtJhs" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         )
     }
