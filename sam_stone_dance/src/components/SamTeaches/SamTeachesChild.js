3604b43594a5501cc0a5c6a7b507c01b4bf45fa0
Meredith Bashert <merbashert@gmail.com>
2020-05-03 20:35:20 -0400

    Added button to Sam Child, appearance notes

--- sam_stone_dance/src/components/SamTeaches/SamTeachesChild.js
+++ sam_stone_dance/src/components/SamTeaches/SamTeachesChild.js
@@ -33,13 +33,21 @@ class SamTeachesChild extends React.Component {
                     <h3>Creative Dance For Kids</h3>
                     <hr />
                     <img src = {image} alt = 'Sam black and white' className = 'child-teach-photo'/>
-                <h6 dangerouslySetInnerHTML = {{__html: childTeach.content.rendered}}></h6>
-                </div>
-            )
+                    <h6 dangerouslySetInnerHTML = {{__html: childTeach.content.rendered}}></h6>
+                    <div id='button-container'>
+                        <h6 id='class-button'>
+                            <a href='https://www.youtube.com/channel/UCGEu46ySZPvZM4XlON77PlA?view_as=subscriber' target="_blank" rel="noopener noreferrer">
+                                Check out Sam's Creative Dance Lessons Online
+                            </a></h6>
+
+                        </div>
+
+                    </div>
+                )
+            }
+            return <div id='loading'><h3>Loading...</h3></div>
+
         }
-        return <div id='loading'><h3>Loading...</h3></div>
-
     }
-}
 
-export default SamTeachesChild;
+    export default SamTeachesChild;
