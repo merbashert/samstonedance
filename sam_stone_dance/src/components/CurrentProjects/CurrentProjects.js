1535cba4de295c5e4bb42e4864b11f0d7541dee6
Meredith Bashert <merbashert@gmail.com>
2020-05-05 10:05:21 -0400

    Added no events collage

--- sam_stone_dance/src/components/CurrentProjects/CurrentProjects.js
+++ sam_stone_dance/src/components/CurrentProjects/CurrentProjects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CurrentProject from './CurrentProject';
 import axios from 'axios';
 
+import collage from '../../assets/collage.jpg'
 import './CurrentProjects.css'
 
 export class CurrentProjects extends React.Component {
@@ -30,7 +31,12 @@ export class CurrentProjects extends React.Component {
                 <div className='current-projects'>
                     <h2>For up-to-date news, visit the <a href = 'https://www.facebook.com/Sam-Stone-Dance-108382910851063' target="_blank" rel="noopener noreferrer">Sam Stone Dance Facebook page</a></h2>
                     {currentProjects.length < 1 ?
-                        <div>
+                        <div className='no-events' style={{
+                            backgroundImage: `url(${collage})`,
+                            backgroundPosition: 'center',
+                            backgroundSize: 'contain',
+                            backgroundRepeat: 'no-repeat'
+                        }}>
                             <h1>Stay tuned!</h1>
                         </div>
                         :
