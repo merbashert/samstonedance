38835131720e13783fbf77b06a8bf372d976f408
Meredith Bashert <merbashert@gmail.com>
2020-05-11 15:58:49 -0400

    Fixed contact responsiveness

--- sam_stone_dance/src/components/Contact/Contact.js
+++ sam_stone_dance/src/components/Contact/Contact.js
@@ -4,7 +4,7 @@ import moon from '../../assets/moon.png'
 import './Contact.css'
 
 const contact = () =>  {
-    
+
     return(
         <div className='contact-container'>
             <img src = {moon} className = 'moon_drop' id='moon_contact' alt='moon'></img>
@@ -12,10 +12,10 @@ const contact = () =>  {
                 <h4>reach Sam at:</h4>
                 <br/>
                 <h4 className='mail' style={{marginLeft: '0px'}}>
-                    <span className='fa fa-envelope-o' style={{fontSize: '35px'}}>
+                    <span className='fa fa-envelope-o' id='contact-envelope'>
                     </span><a href = 'mailto:samstonedance@gmail.com'>samstonedance@gmail.com</a></h4>
 
-                    <h4 className='instagram'><span className='fa fa-instagram'></span><a href = 'https://www.instagram.com/otherdance' target="_blank" rel="noopener noreferrer">@otherdance</a></h4>
+                <h4 className='instagram'><span className='fa fa-instagram' id='contact-instagram'></span><a href = 'https://www.instagram.com/otherdance' target="_blank" rel="noopener noreferrer">@otherdance</a></h4>
 
 
                 </div>
